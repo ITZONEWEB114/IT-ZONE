81fd6bbc5f97369e7f0dc343fcc7fa7e4275801a
Zeng Shao Bin <ITZONEWEB114@126.com>
2011-07-15 15:45:12 +0800

    build the origin project

---
+++ web114/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Web114::Application.config.secret_token = '5e1054b2ddca4c534d67aec6208a07f812dde82810368c4d0555f4a2a8140be9bcd9ee6dc6ca695dc2291957c30f12c930c9b5ae2a9ae46374b9b1cac6930dc9'
