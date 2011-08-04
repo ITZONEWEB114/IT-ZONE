7b49cec886cbe10c9988383be31b64bee9138868
zengshaobin <249505968@qq.com>
2011-08-04 09:49:38 +0800

    Authenticating Users parts finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 ITZONE::Application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
   resources :users
-  resources :line_items
   resources :orders
+  resources :line_items
   resources :carts
-
   get "store/index"
-
   resources :products do
     get :who_bought, :on => :member
   end
