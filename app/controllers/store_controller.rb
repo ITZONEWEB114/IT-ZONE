5528eaf5a0611f79961cd8d5d5b1b37544597f22
Min He <410152019@qq.com>
2011-08-11 14:39:47 +0800

    change the view of store index

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,14 +2,13 @@ class StoreController < ApplicationController
   skip_before_filter :authorize
   skip_before_filter :customer_authorize
   def index
-	
-	
-	if params[:set_locale]
-	  redirect_to store_path(:locale => params[:set_locale])
-	else
-	  @products = Product.all
-    @cart = current_cart
-	end
+	 if params[:set_locale]
+	    redirect_to store_path(:locale => params[:set_locale])
+	 else
+	    @products = Product.all
+	    @hotProducts = Product.find([1,2]);
+      @cart = current_cart
+	 end
   end
 
 end
