925c136cab87eda1b62cdc835f48fd11bb9538fb
zengshaobin <249505968@qq.com>
2011-08-12 09:20:02 +0800

    fix the "español" name text

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -3,7 +3,7 @@ I18n.default_locale = :en
 
 LANGUAGES = [
 	['English',						'en'],
-	["Espa&ntilde;o1".html_safe,	'es'],
+	["Español".html_safe,	'es'],
 	["简体中文".html_safe, 'zh'],
 	["日本语".html_safe, 'jp'],
 	["한국의".html_safe, 'kr']
