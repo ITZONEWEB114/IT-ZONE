09db12ad93c5e90c370abd6de11a3fa50835485d
zengshaobin <249505968@qq.com>
2011-08-12 09:10:45 +0800

    add three language to the config

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -3,5 +3,8 @@ I18n.default_locale = :en
 
 LANGUAGES = [
 	['English',						'en'],
-	["Espa&ntilde;o1".html_safe,	'es']
+	["Espa&ntilde;o1".html_safe,	'es'],
+	["简体中文".html_safe, 'zh'],
+	["日本语".html_safe, 'jp'],
+	["한국의".html_safe, 'kr']
 ] 
